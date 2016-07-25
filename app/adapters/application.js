9977a8108065e2d614804b1239fd7c987de586cd
Sherif Abdelhamid <sherief@vt.edu>
2016-07-25 16:17:03 -0400

    Sending credentials from front end to back end

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 //import OsfAdapter from './osf-adapter';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import Ember from 'ember';
 
 export default JSONAPIAdapter.extend({
 
@@ -14,5 +15,22 @@ export default JSONAPIAdapter.extend({
       url += '/';
     }
     return url;
+  }, ajax: function(url, method, hash) {
+    hash.crossDomain = true;
+    hash.xhrFields = {withCredentials: true};
+    return this._super(url, method, hash);
   },
+   headers: Ember.computed(function() {
+    var csrftoken = ""
+    try {
+       csrftoken = Ember.get(document.cookie.match(/csrftoken\=([^;]*)/), "1");
+    } catch(e){
+      console.log(e)
+      console.log('no csrftoken present');
+    }
+
+    return {
+      "X-CSRFToken": csrftoken
+    };
+  }).volatile()
 });
