22d3d8ce8764d00b6ccc9a9fb5259b5f22781c2d
rytiggy <rytiggy@gmail.com>
2016-08-08 13:26:37 -0400

    added assign reviewers to accordion

--- app/components/accordion-viewer.js
+++ app/components/accordion-viewer.js
@@ -1,9 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  queryParams: { submission_id: null },
+    model(){
+        return Ember.RSVP.hash( {
+            reviewerall: this.store.findAll( 'reviewer' ),
+            reviewername: this.store.findAll( 'reviewer', { reload: true } ).then( function ( reviewer ) {
+                return reviewer.sortBy( 'name' );
+            } ),
+            revieweremail: this.store.findAll( 'reviewer', { reload: true } ).then( function ( reviewer ) {
+                return reviewer.sortBy( 'email' );
+            } )
+        } );
+    },
+
 didRender(){
 
 },
   actions: {
+          navigate() {
+            this.transitionTo( 'index' );
+        },
+      gotoreviewing(){
+        this.transitionTo( 'reviewslist' );
+      },
+      gotoediting(){
+        this.transitionTo( 'peerdashboard' );
+      },
+        filterdata(){
+            Ember.$( '#filter' ).keyup( function () {
+                var rex = new RegExp( Ember.$( this ).val(), 'i' );
+                Ember.$( '.searchable tr' ).hide();
+                Ember.$( '.searchable tr' ).filter( function () {
+                    return rex.test( Ember.$( this ).text() );
+                } ).show();
+            } );
+        }
  }
 });
