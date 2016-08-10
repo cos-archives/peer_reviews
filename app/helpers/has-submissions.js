7a9cf66d244b93f6f3373e0a182c7ead41598bbf
Sherif Abdelhamid <sherief@vt.edu>
2016-08-10 17:00:55 -0400

    remove unused helpers

--- app/helpers/has-submissions.js
+++
@@ -1,7 +0,0 @@
-import Ember from "ember";
-export function hasSubmissions( params/*, hash*/ ) {
-    var linkedNodes = params[ 0 ];
-    var length = linkedNodes.get( 'length' );
-    return length > 0;
-}
-export default Ember.Helper.helper( hasSubmissions );
--- app/helpers/link-to-osf.js
+++
@@ -1,5 +0,0 @@
-import Ember from "ember";
-export function linkToOsf( params ) {
-    return "https://staging.osf.io/" + params;
-}
-export default Ember.Helper.helper( linkToOsf );
