c4e16654fd4da799ff75c8493edcbf6410574ac2
Sherif Abdelhamid <sherief@vt.edu>
2016-08-10 15:37:55 -0400

    remove routes not required by peer reviews

--- app/router.js
+++ app/router.js
@@ -6,28 +6,18 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-    this.route('conference', { path: '/conference' }, function() {
-        this.route('index', { path:'/:conference_id'}, function() {
-            this.route('index', { path:'/' });
-            this.route('submission', { path: '/submission' });
-        });
-    });
-    this.route('register');
-    this.route('login');
-    this.route('reviews');
-    this.route('evaluation');
-    this.route('evalConfirmation');
-    this.route('assignreview');
-    this.route('reviewslist');
-    this.route('signup');
-    this.route('evaluation-page');
-    this.route('peerdashboard', function() {});
+  this.route('login');
+  this.route('evaluation');
+  this.route('evalConfirmation');
+  this.route('assignreview');
+  this.route('reviewslist');
+  this.route('peerdashboard', function() {});
 
-    //replacement for peerdashboard
-    this.route('editing', function() {
-        this.route('submission', { path: '/submission/:submission_id' });
-        this.route('submissions');
-    });
+  //replacement for peerdashboard
+  this.route('editing', function() {
+      this.route('submission', { path: '/submission/:submission_id' });
+      this.route('submissions');
+  });
 });
 
 export default Router;
